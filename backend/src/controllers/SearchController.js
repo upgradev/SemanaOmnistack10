168333e7624a20ea5048e9706c4b872739512ad2
Cleison França Amorim <cleison.upgrade@gmail.com>
2020-03-05 05:31:48 -0400

    course reactjs

---
+++ backend/src/controllers/SearchController.js
@@ -0,0 +1,32 @@
+const Dev = require('../models/Dev');
+const parseStringAsArray = require('../utils/parsStringAsArray');
+
+
+module.exports = {
+    async index(request, response){
+        //buscar todos devs num raio de 10km
+
+        const {latitude, longitude, techs} = request.query;
+
+        const techsArray = parseStringAsArray(techs);
+
+        const devs = await Dev.find({
+            techs: {
+                $in: techsArray,
+            },
+            location: {
+                $near: {
+                    $geometry: {
+                        type: 'Point',
+                        coordinates: [longitude, latitude]
+                    },
+                    $maxDistance: 10000,
+                }
+            }
+        });
+        
+
+        return response.json({devs})
+
+    }
+}
